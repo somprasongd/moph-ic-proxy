2592921514ae9f275abdd572d6afe3c9e8fd7713
Somprasong Damyos <somprasong.damyos@gmail.com>
2024-03-29 22:54:10 +0700

    fixed: แก้ไขการ login หน้าดู api key

--- src/helper/auth-payload.js
+++ src/helper/auth-payload.js
@@ -9,14 +9,19 @@ function createAuthPayload(username, password, secretKey) {
     hospital_code: config.MOPH_HCODE,
   };
 }
-async function isCurrentAuthPayload(app, username, password) {
+async function isCurrentAuthPayload(app = 'mophic', username, password) {
   const strPayload = await cache.get(`${app}${config.AUTH_PAYLOAD_KEY}`);
   // console.log('strPayload', strPayload);
   if (!strPayload) {
     return false;
   }
+  const secretKey =
+    app === 'mophic' ? config.MOPH_IC_AUTH_SECRET : config.FDH_AUTH_SECRET;
   // console.log('payload', JSON.stringify(createAuthPayload(username, password)));
-  return strPayload === JSON.stringify(createAuthPayload(username, password));
+  return (
+    strPayload ===
+    JSON.stringify(createAuthPayload(username, password, secretKey))
+  );
 }
 
 module.exports = {
